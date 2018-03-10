7c400dcf4e3df8ff8f02462b25ad90a4ce2e8351
Nairit11 <nairit32@gmail.com>
2018-03-11 04:39:59 +0530

    Updated Code Uploaded

--- src/main/java/com/mycompany/jgrapht_warmup/Main.java
+++ src/main/java/com/mycompany/jgrapht_warmup/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.jgrapht_warmup;
 
 /**
@@ -23,8 +18,45 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ImportException {
         // TODO code application logic here
+        if (args.length != 3) {
+            System.out.println("Parameters passed should be of form : input_file person1 person2");
+            return;
+        }
+        
+        File InputFile = new File(args[0]);
+        
+        String person1 = args[1];
+        String person2 = args[2];
+
+        // Importing the graph from input file
+        Graph<String, DefaultEdge> InputGraph = new SimpleDirectedGraph<>(DefaultEdge.class);
+        VertexProvider<String> vp = (label, attributes) -> label;
+        EdgeProvider<String, DefaultEdge> ep = (from, to, label, attributes) -> new DefaultEdge();
+        GraphImporter<String, DefaultEdge> importer = new DOTImporter<>(vp, ep);
+        try {
+            importer.importGraph(InputGraph, InputFile);
+        } catch (ImportException e) {
+            System.out.println("Import of Graph Failed : "+e);
+        }
+        
+        // Checking for cycles in the graph, in case there are cycles present least common ancestor cannot be found
+        CycleDetector<String, DefaultEdge> cd = new CycleDetector<>(InputGraph);
+        if (cd.detectCycles()) {
+            System.out.println("The Graph Contains Cycles");
+            return;
+        }
+        
+        // Getting all least commont ancestors 
+        NaiveLcaFinder<String, DefaultEdge> lcaFinder = new NaiveLcaFinder<>(InputGraph);
+        Set<String> lcas = lcaFinder.findLcas(person1, person2);
+        
+        // Printing the fetched result
+        System.out.println("Least Common Ancestors of "+person1+" and "+ person2+" is/are :");
+        for (String i : lcas) {
+	    System.out.println(i);
+        }
     }
     
 }
