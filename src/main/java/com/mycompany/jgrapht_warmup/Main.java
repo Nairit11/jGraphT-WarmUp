8b99b5622f3a892d92b6f082fdfea904f002e2a7
Nairit11 <nairit32@gmail.com>
2018-03-10 19:52:09 +0530

    Initial Maven Build

---
+++ src/main/java/com/mycompany/jgrapht_warmup/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jgrapht_warmup;
+
+/**
+ *
+ * @author nairit-11
+ */
+import org.jgrapht.Graph;
+import org.jgrapht.alg.CycleDetector;
+import org.jgrapht.alg.NaiveLcaFinder;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.io.*;
+
+import java.io.File;
+import java.util.Set;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
